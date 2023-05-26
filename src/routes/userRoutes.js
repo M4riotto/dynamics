2ad126be287063b8f685479e0e7fb16a9aa0ec88
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-05-26 15:59:18 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,21 +1,21 @@
 import express from 'express'
 import {
   listAllUsers,
-  listId,
+  showId,
   createUser,
   deleteUser,
   deleteId,
   updateUser
 } from '../controllers/userCoutroller.js'
-
+import authenticated from '../middlewares/authenticated.js'
 
 const router = express.Router()
 
 router.get('/', listAllUsers)
-router.get('/:id', listId)
+router.get('/:id', showId)
 router.post('/', createUser)
-router.delete('/', deleteUser)
-router.delete('/:id', deleteId)
-router.put('/', updateUser)
+router.delete('/', authenticated, deleteUser) // DELETE ID FROM BODY JSON
+router.delete('/:id', authenticated, deleteId) // DELETE ID FROM PARAMS
+router.put('/', authenticated, updateUser) // UPDATE
 
 export default router
