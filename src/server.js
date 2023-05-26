7fe68547ea10b23707a2e21a72afbd202c8b96ed
Giovanna Siqueira Nicolau <112205067+iamgiovann4@users.noreply.github.com>
2023-05-26 15:58:59 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import productRoutes from './routes/productRoutes.js'
-import authRoutes from './routes/authRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import authRoutes from './routes/authRoutes.js'
+import errorHandler from './middlewares/errorHandler.js'
+import logger from './middlewares/logger.js'
 import { SERVER } from './config.js'
 
 const app = express()
 const port = SERVER.PORT
 
+app.use(logger)
 app.use(cors())
 app.use(express.json())
 
@@ -17,10 +20,14 @@ app.use('/user/', userRoutes)
 app.use('/auth/', authRoutes)
 
 app.all('*', (req, res) => {
-  res.status(404).send('404 Rota não encontrada!')
   res.status(404).json({ message: '404 Rota não encontrada...' })
 })
 
+app.use(errorHandler)
+
+// Inicia o servidor
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log((new Date()).toLocaleDateString('pt-BR'))
